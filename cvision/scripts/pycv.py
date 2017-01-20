21c2ea2779b522874d09dcd2b6412d1c4fbf2ee5
kirillin <kaartemov@gmail.com>
2017-01-20 19:33:59 +0300

    with dimentions mesuares

--- cvision/scripts/pycv.py
+++ cvision/scripts/pycv.py
@@ -107,16 +107,25 @@ class Recognize:
             # compute the Euclidean distance between the midpoints
             dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
             dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
-            if self.pixelsPerMetric is None:
-                self.pixelsPerMetric = dB / self.WIDTH_PIXEL
-            # compute the size of the object
+            # center of frame
+            x0 = self.imshape_px[1] / 2
+            y0 = self.imshape_px[0] / 2
+            # center of object in center frame coord. system
+            # coord. system is 5th joint of manipulator
+            (xoc, yoc)= self.midpoint(tl, br)
+            xcc = x0 - yoc
+            ycc = y0 - xoc
+            #rospy.loginfo("O(%s; %s) --> O'(%s; %s)", tltrX, tlblY, xcc, ycc)
             """
+                Compute the size of the object.
                 object_diag_mm = object_diag_px * frame_mm / frame_px
                 !!! because angle for horizontal and vertical fields of view are different
                         and we do not know orientation object (still)
             """
             dD = math.sqrt(dA ** 2 + dB ** 2)
-            dimD = dD * self.imshape_mm[2] / self.imshape_px[2]
+            # [mm]
+            ratio = self.imshape_mm[2] / self.imshape_px[2]
+            dimD = dD * ratio
             #rospy.loginfo("O_diag_mm = %s", dimD)
             alpha = math.atan2(dB, dA)
             dimA = dimD * math.sin(alpha)
@@ -125,7 +134,8 @@ class Recognize:
             # TODO detecting and analezing objects
             o.shape = 'undefined'
             o.dimensions = (dimA, dimB, 1)
-            o.coordinates = (0, 0, 0)
+            o.coordinates_center_frame = (xcc*ratio*0.001, ycc*ratio*0.001, 0)
+            o.coordinates_frame = (tltrX*ratio*0.001, tlblY*ratio*0.001, 0)
             #info = "Height:%d\tWidth:%d\n" % (dimA, dimB)
             #rospy.loginfo(info)
             list.append(o)
@@ -142,15 +152,25 @@ class Recognize:
                     cv2.circle(image, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
                     cv2.circle(image, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
                     cv2.circle(image, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+                    cv2.circle(image, (int(x0), int(y0)), 5, (0, 255, 0), -1)
+                    
                     # draw lines between the midpoints
                     cv2.line(image, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)), (255, 0, 255), 2)
                     cv2.line(image, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)), (255, 0, 255), 2)
+                    cv2.putText(image, "{:.1f}px;{:.1f}px".format(self.imshape_px[0], self.imshape_px[1]),
+                                (int(0 + 15), int(0 + 20)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+                    cv2.putText(image, "{:.1f}mm;{:.1f}mm".format(self.imshape_mm[0], self.imshape_mm[1]),
+                                ((0 + 15), int(0 + 50)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+                    
+
                     # draw the object sizes on the image
                     cv2.putText(image, "{:.1f}px;{:.1f}mm".format(dA, dimA), (int(tltrX - 15), int(tltrY - 10)),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 0, 0), 2)
                     cv2.putText(image, "{:.1f}px;{:.1f}mm".format(dB, dimB), (int(trbrX + 10), int(trbrY)),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 0, 0), 2)
                     # ------------------
+                    
         if not detail:
             return list
         else:
